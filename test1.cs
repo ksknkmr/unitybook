dacda4b42f5f08130553e82ca04eb05dc9ce5389
ksknkmr <ksknkmr59@gmail.com>
2018-09-07 17:18:23 +0900

    20180906

---
+++ test1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+    // Use this for initialization
+    void Start(){
+        int[] array = new int[5];
+
+        array[0] = 2;
+        array[1] = 10;
+        array[2] = 5;
+        array[3] = 15;
+        array[4] = 3;
+
+        for (int i = 0; i < 5; i++){
+            Debug.Log(array[i]);
+        }
+    }
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
