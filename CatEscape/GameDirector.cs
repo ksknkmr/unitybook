661311693e7e9e09bb860f418462b85afe7f0bcd
ksknkmr <ksknkmr59@gmail.com>
2018-09-18 23:26:20 +0900

    20180918

---
+++ CatEscape/GameDirector.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // UI使うときは忘れない
+
+public class GameDirector : MonoBehaviour {
+
+    GameObject hpGauge;
+
+	// Use this for initialization
+	void Start () {
+        this.hpGauge = GameObject.Find("hpGauge");
+	}
+	
+    public void DecreaseHp(){
+        this.hpGauge.GetComponent<Image>().fillAmount -= 0.1f;
+    }
+}
