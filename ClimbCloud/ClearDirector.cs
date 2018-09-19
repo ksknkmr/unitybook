b852f8ef070147614f1edcf24855841626b045b9
ksknkmr <ksknkmr59@gmail.com>
2018-09-19 14:40:49 +0900

    20180919

---
+++ ClimbCloud/ClearDirector.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // LoadSneceを使うために必要!!
+
+public class ClearDirector : MonoBehaviour {
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetMouseButtonDown(0)){
+            SceneManager.LoadScene("GameScene");
+        }
+	}
+}
